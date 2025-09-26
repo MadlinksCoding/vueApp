509bff8cdf9f915cff211d0adce4eb0e8989a9ac
Ardian Brian <ardianbrianpratama@gmail.com>
2025-09-26 16:46:54 +0700

    update route dashboard

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,7 @@ const auth = useAuthStore();
           Log In
         </button>
       </router-link>
-      <router-link to="/dashboard/settings">
+      <router-link to="/dashboard">
         <button
           style="
             padding: 0.5rem 1rem;
